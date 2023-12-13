852dc2fd802b5e33ae4b671955cf63cb847ba480
yocchan-git <yocchan.business@gmail.com>
2023-12-13 19:07:43 +0900

    メソッドを２つ定義して、ファイルが少ないときに空行ができないようにする

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,12 +1,26 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-file_or_directory = Dir.entries('.').reject { |file| file.match(/^\./) }.sort
-max_length = file_or_directory.max_by(&:length)&.length
+ROW_LENGTH = 4
 
-grouped_files_or_directories = file_or_directory.each_slice(4).map { |slice| slice.fill(nil, slice.length...4) }
-transposed_groups = grouped_files_or_directories.transpose
+def max_length(check_array)
+  check_array.max_by(&:length)&.length
+end
+
+def grouped_files_or_directories(files_or_directories, row_length)
+  files_or_directories.each_slice(row_length).map { |slice| slice.fill(nil, slice.length...row_length) }
+end
+
+file_or_directory = Dir.entries('.').reject { |file| file.match(/^\./) }.sort
+if file_or_directory.length < ROW_LENGTH
+  file_or_directory.each do |file_or_directory_output|
+    puts file_or_directory_output
+  end
+  exit
+end
+
+transposed_groups = grouped_files_or_directories(file_or_directory, ROW_LENGTH).transpose
 transposed_groups.each do |group|
-  group.compact.each { |item| printf("%-#{max_length}s   ", item) }
+  group.compact.each { |item| printf("%-#{max_length(file_or_directory)}s   ", item) }
   puts
 end
