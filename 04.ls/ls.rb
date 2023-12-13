bf84ab1c56e0649facea051b11b6ef9d47fe7f6b
yocchan-git <yocchan.business@gmail.com>
2023-12-13 17:28:13 +0900

    ファイルやフォルダがない時のために修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 file_or_directory = Dir.entries('.').reject { |file| file.match(/^\./) }.sort
-max_length = file_or_directory.max_by(&:length).length
+max_length = file_or_directory.max_by(&:length)&.length
 
 grouped_files_or_directories = file_or_directory.each_slice(4).map { |slice| slice.fill(nil, slice.length...4) }
 transposed_groups = grouped_files_or_directories.transpose
