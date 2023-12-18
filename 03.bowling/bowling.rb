68207b9a07fc53de7a32a951daa49191d71200e4
yocchan-git <yocchan.business@gmail.com>
2023-12-18 09:46:05 +0900

    代入の仕方、条件分岐を一つにまとめた

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -15,22 +15,21 @@ end
 
 frames = shots.each_slice(2).to_a
 point = 0
-throw_third_last_frame = false
 
 if frames.size > 10
   first_strike = frames[9][0] == 10
   second_strike = frames[10][0] == 10
 
-  last_frame = if first_strike && second_strike
-                 [10, 10, frames[11][0]]
-               elsif first_strike
-                 [10, frames[10][0], frames[10][1]]
-               else
-                 [frames[9][0], frames[9][1], frames[10][0]]
-               end
+  last_frame =
+    if first_strike && second_strike
+      [10, 10, frames[11][0]]
+    elsif first_strike
+      [10, frames[10][0], frames[10][1]]
+    else
+      [frames[9][0], frames[9][1], frames[10][0]]
+    end
 
   frames = [*frames[0..8], last_frame]
-  throw_third_last_frame = true
 end
 
 frames.each_with_index do |frame, i|
@@ -38,17 +37,12 @@ frames.each_with_index do |frame, i|
   spare = !strike && frame.sum == 10
   next_frame = frames[i + 1]
   previous_frame = frames[i - 1]
-  throw_first_frame = i.zero?
-  throw_last_frame = i == 9
+  throw_after_first_frame = !i.zero?
+  throw_before_last_frame = i != 9
 
-  if throw_last_frame && throw_third_last_frame
-    point += last_frame.sum
-    next
-  end
-
-  if strike
+  if strike && throw_before_last_frame
     point += 10 + next_frame[0] + next_frame[1]
-    point += next_frame[0] if previous_frame[0] == 10 && !throw_first_frame
+    point += next_frame[0] if previous_frame[0] == 10 && throw_after_first_frame
   elsif spare
     point += 10 + next_frame[0]
   else
