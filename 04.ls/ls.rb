d44a18462dec55f100b1bba05233923d04b3caf1
yocchan-git <yocchan.business@gmail.com>
2023-12-20 16:15:47 +0900

    横に最大３列を維持するよう修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,25 +1,28 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ROW_LENGTH = 4
+ROW_LENGTH = 3
 
 def max_length(check_array)
   check_array.max_by(&:length).length
 end
 
-def grouped_files_or_directories(files_or_directories, row_length)
-  files_or_directories.each_slice(row_length).map { |slice| slice.fill(nil, slice.length...row_length) }
+def grouped_files_or_directories(files_or_directories, column_length)
+  files_or_directories.each_slice(column_length).map { |slice| slice.fill(nil, slice.length...column_length) }
 end
 
 file_or_directory = Dir.entries('.').reject { |file| file.match(/^\./) }.sort
-if file_or_directory.length < ROW_LENGTH
+column_length = file_or_directory.size / ROW_LENGTH
+column_length += 1 unless (file_or_directory.size % ROW_LENGTH).zero?
+
+if file_or_directory.length < column_length
   file_or_directory.each do |file_or_directory_output|
     puts file_or_directory_output
   end
   exit
 end
 
-transposed_groups = grouped_files_or_directories(file_or_directory, ROW_LENGTH).transpose
+transposed_groups = grouped_files_or_directories(file_or_directory, column_length).transpose
 transposed_groups.each do |group|
   group.compact.each { |item| printf("%-#{max_length(file_or_directory)}s   ", item) }
   puts
