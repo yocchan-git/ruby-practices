9cf5c638648aa46ef601065ffa61b16c32d6a0ed
yocchan-git <yocchan.business@gmail.com>
2023-12-23 14:58:41 +0900

    -aと-Aオプションを作成

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,7 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
 ROW_LENGTH = 3
+
+regex_file = /^\./
+OptionParser.new do |opts|
+  opts.on('-a [VAL]') { |_v| regex_file = nil }
+  opts.on('-A [VAL]') { |_v| regex_file = /^\.\.?$/ }
+end.parse!
+
+def create_file_or_directory(regex_file)
+  regex_file.nil? ? Dir.entries('.') : Dir.entries('.').reject { |file| file.match(regex_file) }
+end
 
 def max_length(check_array)
   check_array.max_by(&:length).length
@@ -11,7 +22,7 @@ def grouped_files_or_directories(files_or_directories, column_length)
   files_or_directories.each_slice(column_length).map { |slice| slice.fill(nil, slice.length...column_length) }
 end
 
-file_or_directory = Dir.entries('.').reject { |file| file.match(/^\./) }.sort
+file_or_directory = create_file_or_directory(regex_file).sort
 
 column_length, column_length_remainder = file_or_directory.length.divmod(ROW_LENGTH)
 column_length += 1 unless column_length_remainder.zero?
