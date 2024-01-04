695372a54d1a017a68d7d60639b582521f55a524
yocchan-git <yocchan.business@gmail.com>
2024-01-04 18:21:48 +0900

    レビューを受けて修正しました

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -8,8 +8,16 @@ ROW_LENGTH = 3
 NORMAL_PERMISSIONS_DIGITS = -3..-1
 SPECIAL_PERMISSION_DIGIT = -4
 OTHER_PERMISSION_MOVE = -1
-HARD_LINKS = 1
-SIZE = 4
+PERMISSION_SYMBOLICS = {
+  '0' => '---',
+  '1' => '--x',
+  '2' => '-w-',
+  '3' => '-wx',
+  '4' => 'r--',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx'
+}.freeze
 
 opt = OptionParser.new
 option = nil
@@ -17,18 +25,11 @@ opt.on('-l [VAL]') { |_v| option = :detail }
 opt.parse!(ARGV)
 
 def create_symbolic_permissions(permission_digits)
-  permission_digits.chars.map do |char|
-    {
-      '0' => '---',
-      '1' => '--x',
-      '2' => '-w-',
-      '3' => '-wx',
-      '4' => 'r--',
-      '5' => 'r-x',
-      '6' => 'rw-',
-      '7' => 'rwx'
-    }[char]
-  end.join
+  permission_digits.chars.map { |char| PERMISSION_SYMBOLICS[char] }.join
+end
+
+def find_symlink_file(file_name)
+  'l' if File.lstat(file_name).symlink?
 end
 
 def find_file_type(file_stat)
@@ -36,7 +37,6 @@ def find_file_type(file_stat)
   return 'd' if file_stat.directory?
   return 'c' if file_stat.chardev?
   return 'b' if file_stat.blockdev?
-  return 'l' if file_stat.symlink?
   return 's' if file_stat.socket?
   return 'p' if file_stat.pipe?
 
@@ -52,29 +52,20 @@ def create_special_permission(special_digits)
   end
 end
 
-def create_file_details(file_name)
-  file_stat = File::Stat.new(file_name)
+def create_permission(file_stat)
   permissions = file_stat.mode
 
   permission_digits = format('%o', permissions)[NORMAL_PERMISSIONS_DIGITS]
   special_digits = format('%o', permissions)[SPECIAL_PERMISSION_DIGIT]
 
   symbolic_permissions = create_symbolic_permissions(permission_digits)
-  file_type = find_file_type(file_stat)
   special_permission = create_special_permission(special_digits)
 
   unless special_permission.empty?
     symbolic_permissions[OTHER_PERMISSION_MOVE] = symbolic_permissions[OTHER_PERMISSION_MOVE] == 'x' ? special_permission : special_permission.upcase
   end
 
-  @total += file_stat.blocks
-  @type_and_permissions << "#{file_type}#{symbolic_permissions}"
-  @hard_link << file_stat.nlink.to_s
-  @owner << Etc.getpwuid(file_stat.uid).name
-  @group << Etc.getgrgid(file_stat.gid).name
-  @size << file_stat.size.to_s
-  @created_at << file_stat.birthtime.strftime('%m %d %H:%M')
-  @file_name << file_name
+  symbolic_permissions
 end
 
 def create_max_length(check_files_or_details)
@@ -89,31 +80,41 @@ def grouped_files_or_directories(files_or_directories, column_length)
   files_or_directories.each_slice(column_length).map { |slice| slice.fill(nil, slice.length...column_length) }
 end
 
+def create_file_name(file_name)
+  find_symlink_file(file_name) ? "#{file_name} -> #{File.readlink(file_name)}" : file_name
+end
+
+def create_file_detail(file_name, file_stat)
+  file_or_directory_detail = {}
+
+  file_or_directory_detail[:type_and_permission] = (find_symlink_file(file_name) || find_file_type(file_stat)) + create_permission(file_stat)
+  file_or_directory_detail[:hard_link] = file_stat.nlink.to_s
+  file_or_directory_detail[:owner] = Etc.getpwuid(file_stat.uid).name
+  file_or_directory_detail[:group] = Etc.getgrgid(file_stat.gid).name
+  file_or_directory_detail[:file_size] = file_stat.size.to_s
+  file_or_directory_detail[:updated_at] = file_stat.mtime.strftime('%m %d %H:%M')
+  file_or_directory_detail[:file_name] = create_file_name(file_name)
+
+  file_or_directory_detail
+end
+
 file_or_directory = Dir.entries('.').reject { |file| file.match(/^\./) }.sort
 
 if option == :detail
-  @file_or_directory_details = []
-  @total = 0
-  @type_and_permissions = []
-  @hard_link = []
-  @owner = []
-  @group = []
-  @size = []
-  @created_at = []
-  @file_name = []
+  file_or_directory_details = []
+  total_blocks = 0
 
   file_or_directory.each do |file|
-    create_file_details(file)
+    file_stat = File::Stat.new(file)
+    total_blocks += file_stat.blocks
+
+    file_or_directory_details << create_file_detail(file, file_stat)
   end
 
-  @file_or_directory_details = [@type_and_permissions, @hard_link, @owner, @group, @size, @created_at, @file_name]
-  max_lengths = create_max_lengths(@file_or_directory_details)
-
-  puts "total #{@total}"
-  transposed_groups = @file_or_directory_details.transpose
-  transposed_groups.each do |group|
-    group.compact.each_with_index do |item, index|
-      print [HARD_LINKS, SIZE].include?(index) ? item.rjust(max_lengths[index]) : item.ljust(max_lengths[index])
+  puts "total #{total_blocks}"
+  file_or_directory_details.each do |file_detail|
+    file_detail.each do |key, value|
+      print value
       print '  '
     end
     puts
