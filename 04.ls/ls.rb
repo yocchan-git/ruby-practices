3a26567d73b90d155f0e0fcbd32b83dffa59d612
yocchan-git <yocchan.business@gmail.com>
2024-01-05 22:02:13 +0900

    rubocopパスしたリファクタリング前のコード

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'debug'
 require 'optparse'
 require 'etc'
 
@@ -8,6 +9,8 @@ ROW_LENGTH = 3
 NORMAL_PERMISSIONS_DIGITS = -3..-1
 SPECIAL_PERMISSION_DIGIT = -4
 OTHER_PERMISSION_MOVE = -1
+HARD_LINKS = 1
+FILE_SIZE = 4
 PERMISSION_SYMBOLICS = {
   '0' => '---',
   '1' => '--x',
@@ -76,60 +79,78 @@ def create_max_lengths(files_or_details)
   files_or_details.map { |file_or_detail| create_max_length(file_or_detail.compact) }
 end
 
-def grouped_files_or_directories(files_or_directories, column_length)
-  files_or_directories.each_slice(column_length).map { |slice| slice.fill(nil, slice.length...column_length) }
-end
-
 def create_file_name(file_name)
   find_symlink_file(file_name) ? "#{file_name} -> #{File.readlink(file_name)}" : file_name
 end
 
-def create_file_detail(file_name, file_stat)
-  file_or_directory_detail = {}
+def create_file_details(files)
+  total_blocks = 0
+  type_and_permissions = []
+  hard_links = []
+  owners = []
+  groups = []
+  file_sizes = []
+  updated_times = []
+  file_names = []
 
-  file_or_directory_detail[:type_and_permission] = (find_symlink_file(file_name) || find_file_type(file_stat)) + create_permission(file_stat)
-  file_or_directory_detail[:hard_link] = file_stat.nlink.to_s
-  file_or_directory_detail[:owner] = Etc.getpwuid(file_stat.uid).name
-  file_or_directory_detail[:group] = Etc.getgrgid(file_stat.gid).name
-  file_or_directory_detail[:file_size] = file_stat.size.to_s
-  file_or_directory_detail[:updated_at] = file_stat.mtime.strftime('%m %d %H:%M')
-  file_or_directory_detail[:file_name] = create_file_name(file_name)
+  files.each do |file|
+    file_stat = File::Stat.new(file)
+    total_blocks += file_stat.blocks
 
-  file_or_directory_detail
+    type_and_permissions << (find_symlink_file(file) || find_file_type(file_stat)) + create_permission(file_stat)
+    hard_links << file_stat.nlink.to_s
+    owners << Etc.getpwuid(file_stat.uid).name
+    groups << Etc.getgrgid(file_stat.gid).name
+    file_sizes << file_stat.size.to_s
+    updated_times << file_stat.mtime.strftime('%m %d %H:%M')
+    file_names << create_file_name(file)
+  end
+
+  [total_blocks, [type_and_permissions, hard_links, owners, groups, file_sizes, updated_times, file_names]]
+end
+
+def show_file_details(file_or_directory)
+  total_blocks, file_or_directory_details = create_file_details(file_or_directory)
+  max_lengths = create_max_lengths(file_or_directory_details)
+
+  display_file_details(total_blocks, file_or_directory_details, max_lengths)
+end
+
+def display_file_details(total, file_details, max_lengths)
+  puts "total #{total}"
+
+  transposed_groups = file_details.transpose
+  transposed_groups.each do |group|
+    group.compact.each_with_index do |item, index|
+      print [HARD_LINKS, FILE_SIZE].include?(index) ? item.rjust(max_lengths[index]) : item.ljust(max_lengths[index])
+      print '  '
+    end
+    puts
+  end
+end
+
+def show_file_or_directory(file_or_directory)
+  column_length, column_length_remainder = file_or_directory.length.divmod(ROW_LENGTH)
+  column_length += 1 unless column_length_remainder.zero?
+
+  groups = file_or_directory.each_slice(column_length).map { |slice| slice.fill(nil, slice.length...column_length) }
+  max_lengths = create_max_lengths(groups)
+
+  display_file_or_directory(groups, max_lengths)
+end
+
+def display_file_or_directory(files_or_directories, max_lengths)
+  transposed_groups = files_or_directories.transpose
+  transposed_groups.each do |group|
+    group.compact.each_with_index { |item, index| printf("%-#{max_lengths[index]}s   ", item) }
+    puts
+  end
 end
 
 file_or_directory = Dir.entries('.').reject { |file| file.match(/^\./) }.sort
 
 if option == :detail
-  file_or_directory_details = []
-  total_blocks = 0
-
-  file_or_directory.each do |file|
-    file_stat = File::Stat.new(file)
-    total_blocks += file_stat.blocks
-
-    file_or_directory_details << create_file_detail(file, file_stat)
-  end
-
-  puts "total #{total_blocks}"
-  file_or_directory_details.each do |file_details|
-    file_details.each_value do |file_detail|
-      print file_detail
-      print '  '
-    end
-    puts
-  end
-  exit
-end
-
-column_length, column_length_remainder = file_or_directory.length.divmod(ROW_LENGTH)
-column_length += 1 unless column_length_remainder.zero?
-
-groups = grouped_files_or_directories(file_or_directory, column_length)
-max_lengths = create_max_lengths(groups)
-
-transposed_groups = groups.transpose
-transposed_groups.each do |group|
-  group.compact.each_with_index { |item, index| printf("%-#{max_lengths[index]}s   ", item) }
-  puts
+  show_file_details(file_or_directory)
+else
+  show_file_or_directory(file_or_directory)
 end
