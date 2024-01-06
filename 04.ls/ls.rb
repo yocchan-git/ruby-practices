d7657083221e85439b29dd211e40ecdaec25f764
yocchan-git <yocchan.business@gmail.com>
2024-01-06 10:51:30 +0900

    rubocop修正前の正常動作

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'debug'
 require 'optparse'
 require 'etc'
 
@@ -26,6 +25,14 @@ opt = OptionParser.new
 option = nil
 opt.on('-l [VAL]') { |_v| option = :detail }
 opt.parse!(ARGV)
+
+def create_max_length(check_files_or_details)
+  check_files_or_details.max_by(&:length).length
+end
+
+def create_max_lengths(files_or_details)
+  files_or_details.map { |file_or_detail| create_max_length(file_or_detail.compact) }
+end
 
 def create_symbolic_permissions(permission_digits)
   permission_digits.chars.map { |char| PERMISSION_SYMBOLICS[char] }.join
@@ -71,16 +78,12 @@ def create_permission(file_stat)
   symbolic_permissions
 end
 
-def create_max_length(check_files_or_details)
-  check_files_or_details.max_by(&:length).length
-end
-
-def create_max_lengths(files_or_details)
-  files_or_details.map { |file_or_detail| create_max_length(file_or_detail.compact) }
-end
-
 def create_file_name(file_name)
   find_symlink_file(file_name) ? "#{file_name} -> #{File.readlink(file_name)}" : file_name
+end
+
+def create_updated_time(file_stat)
+  file_stat.mtime.strftime('%m %d').gsub(/\b0(\d)\b/, ' \1') + ' ' + file_stat.mtime.strftime('%H:%M')
 end
 
 def create_file_details(files)
@@ -94,7 +97,7 @@ def create_file_details(files)
   file_names = []
 
   files.each do |file|
-    file_stat = File::Stat.new(file)
+    file_stat = find_symlink_file(file) ? File.lstat(file) : File::Stat.new(file)
     total_blocks += file_stat.blocks
 
     type_and_permissions << (find_symlink_file(file) || find_file_type(file_stat)) + create_permission(file_stat)
@@ -102,18 +105,11 @@ def create_file_details(files)
     owners << Etc.getpwuid(file_stat.uid).name
     groups << Etc.getgrgid(file_stat.gid).name
     file_sizes << file_stat.size.to_s
-    updated_times << file_stat.mtime.strftime('%m %d %H:%M')
+    updated_times << create_updated_time(file_stat)
     file_names << create_file_name(file)
   end
 
   [total_blocks, [type_and_permissions, hard_links, owners, groups, file_sizes, updated_times, file_names]]
-end
-
-def show_file_details(file_or_directory)
-  total_blocks, file_or_directory_details = create_file_details(file_or_directory)
-  max_lengths = create_max_lengths(file_or_directory_details)
-
-  display_file_details(total_blocks, file_or_directory_details, max_lengths)
 end
 
 def display_file_details(total, file_details, max_lengths)
@@ -129,14 +125,11 @@ def display_file_details(total, file_details, max_lengths)
   end
 end
 
-def show_file_or_directory(file_or_directory)
-  column_length, column_length_remainder = file_or_directory.length.divmod(ROW_LENGTH)
-  column_length += 1 unless column_length_remainder.zero?
+def show_file_details(file_or_directory)
+  total_blocks, file_or_directory_details = create_file_details(file_or_directory)
+  max_lengths = create_max_lengths(file_or_directory_details)
 
-  groups = file_or_directory.each_slice(column_length).map { |slice| slice.fill(nil, slice.length...column_length) }
-  max_lengths = create_max_lengths(groups)
-
-  display_file_or_directory(groups, max_lengths)
+  display_file_details(total_blocks, file_or_directory_details, max_lengths)
 end
 
 def display_file_or_directory(files_or_directories, max_lengths)
@@ -145,6 +138,16 @@ def display_file_or_directory(files_or_directories, max_lengths)
     group.compact.each_with_index { |item, index| printf("%-#{max_lengths[index]}s   ", item) }
     puts
   end
+end
+
+def show_file_or_directory(file_or_directory)
+  column_length, column_length_remainder = file_or_directory.length.divmod(ROW_LENGTH)
+  column_length += 1 unless column_length_remainder.zero?
+
+  groups = file_or_directory.each_slice(column_length).map { |slice| slice.fill(nil, slice.length...column_length) }
+  max_lengths = create_max_lengths(groups)
+
+  display_file_or_directory(groups, max_lengths)
 end
 
 file_or_directory = Dir.entries('.').reject { |file| file.match(/^\./) }.sort
