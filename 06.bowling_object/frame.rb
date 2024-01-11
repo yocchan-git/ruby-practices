e7e3e5b43926cd16b959c0e3f0157241be38fcc8
yocchan-git <yocchan.business@gmail.com>
2024-01-11 11:25:32 +0900

    Frameクラスを作成する

---
+++ 06.bowling_object/frame.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Frame
+  def create_frames(formatted_scores)
+    frames = formatted_scores.each_slice(2).to_a
+
+    if frames.size > 10
+      first_strike = frames[9][0] == 10
+      second_strike = frames[10][0] == 10
+    
+      last_frame =
+        if first_strike && second_strike
+          [10, 10, frames[11][0]]
+        elsif first_strike
+          [10, frames[10][0], frames[10][1]]
+        else
+          [frames[9][0], frames[9][1], frames[10][0]]
+        end
+    
+      frames = [*frames[0..8], last_frame]
+    end
+
+    frames
+  end
+end
