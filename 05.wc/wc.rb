e53b1d111ba2ac6b29dbf183733813907fce4c66
yocchan-git <yocchan.business@gmail.com>
2024-01-18 09:21:17 +0900

    リファクタリング完了後

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'debug'
 require 'optparse'
 
 display_bytes = nil
@@ -64,12 +63,8 @@ def create_file_details(file_contents)
   [file_lines, file_bytes, file_words]
 end
 
-def create_max_length(check_files_or_details)
-  check_files_or_details.max_by(&:length).length
-end
-
-def create_max_lengths(files_or_details)
-  files_or_details.map { |file_or_detail| create_max_length(file_or_detail.compact) }
+def create_max_lengths(files_details)
+  files_details.map { |file_detail| file_detail.max_by(&:length).length }
 end
 
 set_files = ARGV
@@ -90,18 +85,16 @@ def create_file_options(is_display_lines, is_display_bytes, is_display_words)
 end
 
 file_options = create_file_options(display_lines, display_bytes, display_words)
-
 file_details = file_options.empty? ? create_file_details(file_contents) : create_file_details_with_options(file_contents, file_options)
 
 set_files << "total" if is_require_total
 file_details << set_files
-
-max_lengths = create_max_lengths(file_details)
-
-display_file_details = file_details.transpose
 FILE_NAMES = file_details.length - 1
 
-display_file_details.each do |display_file_detail|
+max_lengths = create_max_lengths(file_details)
+transformed_file_details = file_details.transpose
+
+transformed_file_details.each do |display_file_detail|
   display_file_detail.each_with_index do |file_detail, index|
     print '  '
     print FILE_NAMES == index ? file_detail.ljust(max_lengths[index]) : file_detail.rjust(max_lengths[index])
