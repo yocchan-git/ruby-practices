3c7557cc8cbaa8454b6235f2454470fd4f5ddf44
yocchan-git <yocchan.business@gmail.com>
2024-01-24 09:48:56 +0900

    main導入と関数の位置を変更する

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -15,6 +15,56 @@ OptionParser.new do |opts|
   opts.on('-c') { |is_option| display_bytes = is_option }
 end.parse!
 
+def main(display_lines, display_words, display_bytes)
+  file_options = create_file_options(display_lines, display_words, display_bytes)
+
+  file_contents = create_file_contents
+  file_details = file_options.empty? ? create_file_details(file_contents) : create_file_details_with_options(file_contents, file_options)
+
+  entered_files = ARGV
+  entered_files << 'total' if require_total?
+  file_details << entered_files unless pipe?
+
+  display_file_details(file_details)
+end
+
+def create_file_options(is_display_lines, is_display_words, is_display_bytes)
+  file_options = []
+
+  file_options << 'lines' if is_display_lines
+  file_options << 'words' if is_display_words
+  file_options << 'bytes' if is_display_bytes
+
+  file_options
+end
+
+def create_file_contents
+  return [$stdin.read] if pipe?
+
+  file_contents_draft = []
+
+  ARGV.each do |file|
+    file_contents_draft << File.read(file)
+  end
+  file_contents_draft
+end
+
+def create_file_details(file_contents)
+  file_lines = create_file_lines(file_contents)
+  file_words = create_file_words(file_contents)
+  file_bytes = create_file_bytes(file_contents)
+
+  [file_lines, file_words, file_bytes]
+end
+
+def create_file_details_with_options(file_contents, options)
+  file_lines = create_file_lines(file_contents) if options.include?('lines')
+  file_words = create_file_words(file_contents) if options.include?('words')
+  file_bytes = create_file_bytes(file_contents) if options.include?('bytes')
+
+  [file_lines, file_words, file_bytes].compact
+end
+
 def require_total?
   !ARGV[1].nil?
 end
@@ -53,64 +103,19 @@ def create_file_words(file_contents)
   file_words.map(&:to_s)
 end
 
-def create_file_details_with_options(file_contents, options)
-  file_lines = create_file_lines(file_contents) if options.include?('lines')
-  file_words = create_file_words(file_contents) if options.include?('words')
-  file_bytes = create_file_bytes(file_contents) if options.include?('bytes')
+def display_file_details(file_details)
+  file_name_number = file_details.length - 1
 
-  [file_lines, file_words, file_bytes].compact
-end
+  max_lengths = file_details.map { |file_detail| file_detail.max_by(&:length).length }
+  transformed_file_details = file_details.transpose
 
-def create_file_details(file_contents)
-  file_lines = create_file_lines(file_contents)
-  file_words = create_file_words(file_contents)
-  file_bytes = create_file_bytes(file_contents)
-
-  [file_lines, file_words, file_bytes]
-end
-
-def create_max_lengths(files_details)
-  files_details.map { |file_detail| file_detail.max_by(&:length).length }
-end
-
-def create_file_contents
-  return [$stdin.read] if pipe?
-
-  file_contents_draft = []
-
-  ARGV.each do |file|
-    file_contents_draft << File.read(file)
+  transformed_file_details.each do |display_file_detail|
+    display_file_detail.each_with_index do |file_detail, index|
+      print '  '
+      print file_name_number === index ? file_detail.ljust(max_lengths[index]) : file_detail.rjust(max_lengths[index])
+    end
+    puts
   end
-  file_contents_draft
 end
 
-entered_files = ARGV
-file_contents = create_file_contents
-
-def create_file_options(is_display_lines, is_display_words, is_display_bytes)
-  file_options = []
-
-  file_options << 'lines' if is_display_lines
-  file_options << 'words' if is_display_words
-  file_options << 'bytes' if is_display_bytes
-
-  file_options
-end
-
-file_options = create_file_options(display_lines, display_words, display_bytes)
-file_details = file_options.empty? ? create_file_details(file_contents) : create_file_details_with_options(file_contents, file_options)
-
-entered_files << 'total' if require_total?
-file_details << entered_files unless pipe?
-FILE_NAMES = file_details.length - 1
-
-max_lengths = create_max_lengths(file_details)
-transformed_file_details = file_details.transpose
-
-transformed_file_details.each do |display_file_detail|
-  display_file_detail.each_with_index do |file_detail, index|
-    print '  '
-    print FILE_NAMES === index ? file_detail.ljust(max_lengths[index]) : file_detail.rjust(max_lengths[index])
-  end
-  puts
-end
+main(display_lines, display_words, display_bytes)
