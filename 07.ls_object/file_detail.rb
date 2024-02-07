c25d17e545bfebcca0c79439eb7ca2216abeb978
yocchan-git <yocchan.business@gmail.com>
2024-02-07 21:48:08 +0900

    FileFormatterクラスを導入する

--- 07.ls_object/file_detail.rb
+++ 07.ls_object/file_detail.rb
@@ -1,37 +1,16 @@
 # frozen_string_literal: true
 
 require 'etc'
+require_relative './file_formatter'
 
 class FileDetail
-  NORMAL_PERMISSIONS_DIGITS = -3..-1
-  SPECIAL_PERMISSION_DIGIT = -4
-  OTHER_PERMISSION_MOVE = -1
   HARD_LINK = 1
   FILE_SIZE = 4
-  PERMISSION_SYMBOLICS = {
-    '0' => '---',
-    '1' => '--x',
-    '2' => '-w-',
-    '3' => '-wx',
-    '4' => 'r--',
-    '5' => 'r-x',
-    '6' => 'rw-',
-    '7' => 'rwx'
-  }.freeze
 
   def initialize(file_paths)
     @file_paths = file_paths
-
     @total_blocks = 0
-    @type_and_permissions = []
-    @hard_links = []
-    @owners = []
-    @groups = []
-    @file_sizes = []
-    @updated_times = []
-    @file_names = []
-
-    set_details
+    @file_details = fetch_file_details
   end
 
   def display
@@ -49,70 +28,28 @@ class FileDetail
 
   private
 
-  def set_details
-    @file_paths.each do |file|
-      @file_name = file
-      @file_stat = find_symlink_file ? File.lstat(file) : File::Stat.new(file)
+  def fetch_file_details
+    type_and_permissions = []
+    hard_links = []
+    owners = []
+    groups = []
+    file_sizes = []
+    updated_times = []
+    file_names = []
 
-      @total_blocks += @file_stat.blocks
-      @type_and_permissions << (find_symlink_file || find_file_type) + create_permission
-      @hard_links << @file_stat.nlink.to_s
-      @owners << Etc.getpwuid(@file_stat.uid).name
-      @groups << Etc.getgrgid(@file_stat.gid).name
-      @file_sizes << @file_stat.size.to_s
-      @updated_times << create_updated_time
-      @file_names << create_file_name
+    @file_paths.each do |file_path|
+      file_formatter = FileFormatter.new(file_path)
+
+      @total_blocks += file_formatter.blocks
+      type_and_permissions << file_formatter.file_type + file_formatter.permission
+      hard_links << file_formatter.hard_link
+      owners << file_formatter.owner
+      groups << file_formatter.group
+      file_sizes << file_formatter.file_size
+      updated_times << file_formatter.updated_time
+      file_names << file_formatter.name
     end
 
-    @file_details = [@type_and_permissions, @hard_links, @owners, @groups, @file_sizes, @updated_times, @file_names]
-  end
-
-  def find_symlink_file
-    'l' if File.lstat(@file_name).symlink?
-  end
-
-  def find_file_type
-    return '-' if @file_stat.file?
-    return 'd' if @file_stat.directory?
-    return 'c' if @file_stat.chardev?
-    return 'b' if @file_stat.blockdev?
-    return 's' if @file_stat.socket?
-    return 'p' if @file_stat.pipe?
-
-    '?'
-  end
-
-  def create_permission
-    permissions = @file_stat.mode
-
-    permission_digits = format('%o', permissions)[NORMAL_PERMISSIONS_DIGITS]
-    @special_digits = format('%o', permissions)[SPECIAL_PERMISSION_DIGIT]
-
-    symbolic_permissions = permission_digits.chars.map { |char| PERMISSION_SYMBOLICS[char] }.join
-    special_permission = create_special_permission
-
-    return symbolic_permissions if special_permission.empty?
-
-    special_permission_symbolic = symbolic_permissions[OTHER_PERMISSION_MOVE] == 'x' ? special_permission : special_permission.upcase
-    symbolic_permissions.gsub(/.$/, special_permission_symbolic)
-  end
-
-  def create_special_permission
-    case @special_digits
-    when '1' then 't'
-    when '2' then 's'
-    when '4' then 's'
-    else ''
-    end
-  end
-
-  def create_updated_time
-    month_and_date = @file_stat.mtime.strftime('%m %d').gsub(/\b0(\d)\b/, ' \1')
-    hour_and_minutes = @file_stat.mtime.strftime('%H:%M')
-    "#{month_and_date} #{hour_and_minutes}"
-  end
-
-  def create_file_name
-    find_symlink_file ? "#{@file_name} -> #{File.readlink(@file_name)}" : @file_name
+    [type_and_permissions, hard_links, owners, groups, file_sizes, updated_times, file_names]
   end
 end
---
+++ 07.ls_object/file_formatter.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+class FileFormatter
+  ROW_LENGTH = 3
+  NORMAL_PERMISSIONS_DIGITS = -3..-1
+  SPECIAL_PERMISSION_DIGIT = -4
+  OTHER_PERMISSION_MOVE = -1
+  PERMISSION_SYMBOLICS = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }.freeze
+
+  def initialize(file_name)
+    @file_name = file_name
+    @file_stat = symlink_file? ? File.lstat(file_name) : File::Stat.new(file_name)
+  end
+
+  def self.fetch_column_file_groups(file_paths)
+    column_length, column_length_remainder = file_paths.length.divmod(ROW_LENGTH)
+    column_length += 1 unless column_length_remainder.zero?
+
+    file_paths.each_slice(column_length).map { |file_path| file_path.fill(nil, file_path.length...column_length) }
+  end
+
+  def blocks
+    @file_stat.blocks
+  end
+
+  def file_type
+    return 'l' if symlink_file?
+    return '-' if @file_stat.file?
+    return 'd' if @file_stat.directory?
+    return 'c' if @file_stat.chardev?
+    return 'b' if @file_stat.blockdev?
+    return 's' if @file_stat.socket?
+    return 'p' if @file_stat.pipe?
+
+    '?'
+  end
+
+  def permission
+    permissions = @file_stat.mode
+
+    permission_digits = format('%o', permissions)[NORMAL_PERMISSIONS_DIGITS]
+    special_digits = format('%o', permissions)[SPECIAL_PERMISSION_DIGIT]
+
+    symbolic_permissions = permission_digits.chars.map { |char| PERMISSION_SYMBOLICS[char] }.join
+    special_permission = find_special_permission(special_digits)
+
+    return symbolic_permissions if special_permission.empty?
+
+    special_permission_symbolic = symbolic_permissions[OTHER_PERMISSION_MOVE] == 'x' ? special_permission : special_permission.upcase
+    symbolic_permissions.gsub(/.$/, special_permission_symbolic)
+  end
+
+  def hard_link
+    @file_stat.nlink.to_s
+  end
+
+  def owner
+    Etc.getpwuid(@file_stat.uid).name
+  end
+
+  def group
+    Etc.getgrgid(@file_stat.gid).name
+  end
+
+  def file_size
+    @file_stat.size.to_s
+  end
+
+  def updated_time
+    month_and_date = @file_stat.mtime.strftime('%m %d').gsub(/\b0(\d)\b/, ' \1')
+    hour_and_minutes = @file_stat.mtime.strftime('%H:%M')
+    "#{month_and_date} #{hour_and_minutes}"
+  end
+
+  def name
+    symlink_file? ? "#{@file_name} -> #{File.readlink(@file_name)}" : @file_name
+  end
+
+  private
+
+  def symlink_file?
+    File.lstat(@file_name).symlink?
+  end
+
+  def find_special_permission(special_digits)
+    case special_digits
+    when '1' then 't'
+    when '2' then 's'
+    when '4' then 's'
+    else ''
+    end
+  end
+end
