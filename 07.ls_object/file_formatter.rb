a64c320a35b7ca3e044b54dd665d8bdbf6ce6127
yocchan-git <yocchan.business@gmail.com>
2024-02-13 20:43:59 +0900

    不要なインスタンス変数を削除する

--- 07.ls_object/file_formatter.rb
+++ 07.ls_object/file_formatter.rb
@@ -5,16 +5,14 @@ class FileFormatter
   FILE_SIZE = 4
 
   def initialize(file_paths)
-    @file_paths = file_paths
-    @total_blocks = 0
-    @file_details = fetch_file_details
+    @file_details = fetch_file_details(file_paths)
   end
 
   def display
-    formatted_file_details = format_file_details
+    total_blocks, formatted_file_details = format_file_details
     max_lengths = formatted_file_details.map { |formatted_file_detail| formatted_file_detail.compact.max_by(&:length).length }
 
-    puts "total #{@total_blocks}"
+    puts "total #{total_blocks}"
     formatted_file_details.transpose.each do |formatted_file_detail|
       formatted_file_detail.compact.each_with_index do |item, index|
         print [HARD_LINK, FILE_SIZE].include?(index) ? item.rjust(max_lengths[index]) : item.ljust(max_lengths[index])
@@ -26,11 +24,12 @@ class FileFormatter
 
   private
 
-  def fetch_file_details
-    @file_paths.map { |file_path| FileDetail.new(file_path) }
+  def fetch_file_details(file_paths)
+    file_paths.map { |file_path| FileDetail.new(file_path) }
   end
 
   def format_file_details
+    total_blocks = 0
     type_and_permissions = []
     hard_links = []
     owners = []
@@ -40,7 +39,7 @@ class FileFormatter
     file_names = []
 
     @file_details.each do |file_detail|
-      @total_blocks += file_detail.blocks
+      total_blocks += file_detail.blocks
       type_and_permissions << file_detail.file_type + file_detail.permission
       hard_links << file_detail.hard_link
       owners << file_detail.owner
@@ -50,6 +49,6 @@ class FileFormatter
       file_names << file_detail.name
     end
 
-    [type_and_permissions, hard_links, owners, groups, file_sizes, updated_times, file_names]
+    [total_blocks, [type_and_permissions, hard_links, owners, groups, file_sizes, updated_times, file_names]]
   end
 end
